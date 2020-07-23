c3f4467bb620bad1b0300bf0ce48be70bece5eab
IttaiBarkai <53575993+IttaiBarkai@users.noreply.github.com>
2020-07-23 16:32:20 +0200

    added migrations and updated crew member model and repo files

--- lib/repos/crew_member.dart
+++ lib/repos/crew_member.dart
@@ -3,13 +3,15 @@ import 'package:olracddl/models/crew_member.dart';
 import 'package:olracddl/providers/database.dart';
 
 class CrewMemberRepo extends DatabaseRepo<CrewMember> {
-  CrewMemberRepo() : super(tableName: 'skippers', database: DatabaseProvider().database);
+  CrewMemberRepo() : super(tableName: 'crew_members', database: DatabaseProvider().database);
 
   @override
   Future<CrewMember> fromDatabaseResult(Map<String, dynamic> result) async {
     return CrewMember(
       id: result['id'],
       name: result['name'],
+      shortName: result['short_name'],
+      seamanId: result['seaman_id'],
       createdAt: DateTime.parse(result['created_at']),
     );
   }
