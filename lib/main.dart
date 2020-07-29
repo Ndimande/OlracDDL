a83c7242bd4859a6d86432eeead5c57bbafa1213
Nqobani <37586727+Ndimande@users.noreply.github.com>
2020-07-29 09:30:31 +0200

    small-changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:migrator/migrator.dart';
 import 'package:olracddl/app_config.dart';
@@ -5,6 +6,7 @@ import 'package:olracddl/app_data.dart';
 import 'package:olracddl/app_migrations.dart';
 import 'package:olracddl/models/profile.dart';
 import 'package:olracddl/providers/database.dart';
+import 'package:olracddl/providers/dio.dart';
 import 'package:olracddl/screens/home.dart';
 import 'package:olracddl/screens/sign_up.dart';
 import 'package:olracddl/screens/splash.dart';
@@ -20,7 +22,7 @@ Future<void> main() async {
   _database = await _databaseProvider.connect();
   final Migrator migrator = Migrator(_database, appMigrations);
   await migrator.run(AppConfig.RESET_DATABASE);
-
+  DioProvider().init();
   runApp(MyApp());
 }
 
@@ -46,13 +48,13 @@ class _MyAppState extends State<MyApp> {
 //      if (!AppConfig.debugMode)
       await Future.delayed(const Duration(seconds: 5));
 
-      if (AppData.profile != null) {
-        await _navigatorKey.currentState
-            .pushReplacement(MaterialPageRoute(maintainState: true, builder: (_) => HomeScreen()));
-      } else {
-        await _navigatorKey.currentState
-            .pushReplacement(MaterialPageRoute(maintainState: false, builder: (_) => SignUpScreen()));
-      }
+//      if (AppData.profile != null) {
+//        await _navigatorKey.currentState
+//            .pushReplacement(MaterialPageRoute(maintainState: true, builder: (_) => HomeScreen()));
+//      } else {
+//        await _navigatorKey.currentState
+//            .pushReplacement(MaterialPageRoute(maintainState: false, builder: (_) => SignUpScreen()));
+//      }
     });
   }
 
@@ -69,7 +71,7 @@ class _MyAppState extends State<MyApp> {
       navigatorKey: _navigatorKey,
       title: AppConfig.APP_TITLE,
       theme: OlracThemesLight.olspsLightTheme,
-      home: SplashScreen(), // Change to the screen you're working on
+      home: HomeScreen(), // Change to the screen you're working on
     );
   }
 }
